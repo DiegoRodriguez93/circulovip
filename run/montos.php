cda58289eab33caa469d85722e86d8a4fbd23e07
Diego Rodriguez <d.rodriguez@vida.com.uy>
2019-11-26 12:44:44 -0300

    c

--- run/montos.php
+++ run/montos.php
@@ -9,7 +9,7 @@ $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 $fecha_hoy = date('Y/m/d');
-$fecha_mañana = date("Y/m/d", strtotime($fecha_hoy . "+ 130 hours"));
+$fecha_mañana = date("Y/m/d", strtotime($fecha_hoy . "+ 24 hours"));
 
 $fecha_carga = substr($fecha_mañana,8);
 
