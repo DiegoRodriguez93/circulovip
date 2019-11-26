b79d451c2d0d2521f0b1d3e41265fbeb0a80f2b3
Diego Rodriguez <d.rodriguez@vida.com.uy>
2019-11-26 12:20:47 -0300

    b

---
+++ run/montos.php
@@ -0,0 +1,26 @@
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$pass = 'sist.2k8';
+$db =   'vidapesos';
+
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+$fecha_hoy = date('Y/m/d');
+$fecha_mañana = date("Y/m/d", strtotime($fecha_hoy . "+ 130 hours"));
+
+$fecha_carga = substr($fecha_mañana,8);
+
+if($fecha_carga = '01'){
+
+$update_dinero = mysqli_query($mysqli,"UPDATE estado_de_cuenta_usuarios
+SET monto = 1000
+WHERE tipo_user = 1 ");
+
+}
+
+mysqli_close($mysqli);
+
+?>
