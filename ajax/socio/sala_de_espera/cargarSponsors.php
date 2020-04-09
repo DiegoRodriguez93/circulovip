995c0e9ac2daaeae498e4fb8cb9601f9ad9f1aac
Diego Rodriguez Paiva <diegorodriguez93@hotmail.com>
2020-04-09 15:24:23 -0300

    231231231231

--- ajax/socio/sala_de_espera/cargarSponsors.php
+++ ajax/socio/sala_de_espera/cargarSponsors.php
@@ -2,7 +2,7 @@
 require '../../_conexion.php';
 
 
-$select3 = mysqli_query($mysqli, "SELECT url_image, href, tipo FROM sponsors");
+$select3 = mysqli_query($mysqli, "SELECT url_image, href, tipo, time FROM sponsors");
 
 if(mysqli_num_rows($select3) > 0){
     while($row3 = mysqli_fetch_array($select3)){
@@ -10,10 +10,12 @@ if(mysqli_num_rows($select3) > 0){
        $url_image = $row3['url_image'];
        $href = $row3['href'];
        $tipo = $row3['tipo'];
+       $time = $row3['time'];
 
        $res[] = array('url_image'=>$url_image,
        'href'=>$href,
-       'tipo'=>$tipo );
+       'tipo'=>$tipo, 
+       'time'=>$time, );
 
     }
 }else{
