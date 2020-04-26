3197875a872c7ade1778ba64ba460c627b8e0231
Diego Rodriguez Paiva <diegorodriguez93@hotmail.com>
2020-04-26 12:57:45 -0300

    123123123123

--- ajax/socio/ver_empresa/rellenarCalendario.php
+++ ajax/socio/ver_empresa/rellenarCalendario.php
@@ -31,6 +31,7 @@ if(mysqli_num_rows($select) == 0){
         
         
         $horarios = '<div class="row" style="background-color: #999">';
+        $arrDeHorarios = '(';
 
 
         // EXTRAEMOS LA ZONA HORARIA RECEPTOR
@@ -68,12 +69,6 @@ if(mysqli_num_rows($select) == 0){
 
             $fecha_a_chequear = $horaFormat2 . ' ' . $horaFormat . ':00';
         
-            $select4 = mysqli_query($mysqli,"SELECT dia_hora, id_receptor, estado 
-            FROM citas WHERE (id_receptor = '$receptor' OR id_emisor = '$receptor')
-             AND dia_hora = '$fecha_a_chequear' AND (estado = 2 OR estado = 1) ");
-        
-             /* if(intval($zona_horaria_receptor) >= 0){} */
-        
             if( intval($zona_horaria_receptor) == intval($zona_horaria_emisor)){
                 $hora_mia =  $horaFormat;
             }else{
@@ -98,20 +93,56 @@ if(mysqli_num_rows($select) == 0){
             }
         
             $fecha_a_chequear2 = $horaFormat2 . ' ' . $hora_mia . ':00';
+
+            $select4 = mysqli_query($mysqli,"SELECT dia_hora, id_receptor, estado 
+            FROM citas WHERE (id_receptor = '$receptor' OR id_emisor = '$receptor')
+             AND dia_hora = '$fecha_a_chequear2' AND (estado = 2 OR estado = 1) ");
         
             $select5 = mysqli_query($mysqli,"SELECT dia_hora, id_receptor, estado 
             FROM citas WHERE (id_receptor = '$emisor' OR id_emisor = '$emisor')
              AND dia_hora = '$fecha_a_chequear2' AND (estado = 2 OR estado = 1)");
+            
+            $hora6 = $hora_mia . ':00';
+
+             $select6 = mysqli_query($mysqli,"SELECT tipo FROM horarios_personalizados
+              WHERE (id_user = '$emisor' OR id_user = '$receptor') 
+              AND dia_de_cita = '$horaFormat2'
+              AND horario = '$hora6' ");
         
             if(mysqli_num_rows($select4) > 0 OR mysqli_num_rows($select5) > 0){
                 // CITA YA SOLICITADA EN ESE HORARIO
                 $estado = 1;
-                $onClick =  'disabled >Solicitar</button>';
-            }else{  
+                $onClick =  'disabled >Ya solicitado</button>';
+            }else{ 
+
+                if(mysqli_num_rows($select6) > 0){
+
+                    $row4 = mysqli_fetch_assoc($select6);
+
+                    if($row4['tipo'] == 0){
+
+                        $estado = 1;
+                        $onClick =  'disabled >Reservado</button>';
+
+                    }else{
+
+                        $onClick =  'onclick="insertarRonda(`'.$fechas_ronda.'`,`'.$receptor.'`,`'.$hora_mia.'`)">Solicitar</button>';
+                        $estado = 0;
+
+                    }
+
+                }else{
+                    
                 $onClick =  'onclick="insertarRonda(`'.$fechas_ronda.'`,`'.$receptor.'`,`'.$hora_mia.'`)">Solicitar</button>';
                 $estado = 0;
-            
+
+                }
+
             }
+
+            $horario7 = $horaFormat . ':00';
+
+            $arrDeHorarios .=  "'".$horario7 . "',";
 
          $horarios .= '<div class="col-4 text-center" style="background-color: #fff;border:1px solid #a3a3a3"><b>
          '.$horaFormat.' hs<br></b>
@@ -125,7 +156,65 @@ if(mysqli_num_rows($select) == 0){
     }
     
 } // fin del while de horarios
+$fecha7 = $row['fecha'];
 
+$arrDeHorarios = substr($arrDeHorarios, 0, -1); // delete last comma
+
+$arrDeHorarios .= ')'; 
+
+$select7 = mysqli_query($mysqli,"SELECT horario, tipo FROM horarios_personalizados 
+WHERE id_user = '$receptor' 
+AND dia_de_cita = '$fecha7'
+AND horario NOT IN $arrDeHorarios AND tipo = 1 ");
+
+if(mysqli_num_rows($select7) > 0){
+
+    while($row5 = mysqli_fetch_array($select7)){
+
+        $date = DateTime::createFromFormat( 'H:i:s', $row5['horario'] );
+        $horaFormat = $date->format( 'H:i'); 
+    
+        // CHEQUEMOS QUE NO ESTE SOLICITADA LA FECHA
+    
+        $horaFormat2 = $row['fecha']; 
+
+        $fecha_a_chequear = $horaFormat2 . ' ' . $horaFormat . ':00';
+    
+        if( intval($zona_horaria_receptor) == intval($zona_horaria_emisor)){
+
+            $hora_mia =  $horaFormat;
+
+        }else{
+    
+         $diferencia_de_horas =   count(range(intval($zona_horaria_receptor),intval($zona_horaria_emisor))) ;
+    
+         $hora = $date->format('H');
+         $minutos = $date->format('i');
+    
+         if(intval($zona_horaria_receptor) > intval($zona_horaria_emisor)){
+    
+            $hora_final = $hora - $diferencia_de_horas + 1;
+    
+         }else{
+    
+            $hora_final = $hora + $diferencia_de_horas - 1;
+    
+         }
+    
+         $hora_mia = $hora_final.':'.$minutos;
+    
+        }
+
+        $horarios .= '<div class="col-4 text-center" style="background-color: #fff;border:1px solid #a600ff"><b>
+        '.$horaFormat.' hs<br></b>
+        <small>Mi horario '.$hora_mia.'</small>
+        <button class="btn btn-sm btn-deep-purple" 
+        onclick="insertarRonda(`'.$fechas_ronda.'`,`'.$receptor.'`,`'.$hora_mia.'`)">Solicitar</button>
+        </div>' ;
+
+    }
+
+}
 
 
 }
